926e0c34706ad5e7dca6dd037094c143b0d78781
ensan-hcl <ensan.hcl.dev@gmail.com>
2025-08-13 22:12:47 +0900

    feat: quick demo features

--- ZenzaiMobile/ContentView.swift
+++ ZenzaiMobile/ContentView.swift
@@ -14,42 +14,65 @@ struct ContentView: View {
     @State private var output = ""
     @State private var exectime = ""
     @State private var converter = KanaKanjiConverter()
+    @State private var zenzaiEnabled = true
+    @State private var zenzaiInferenceLimit = 1
+    @State private var presentInspector = false
     var body: some View {
-        VStack {
-            Text("Zenzai Japanese Input")
-                .font(.title)
-            if !output.isEmpty {
-                Text(verbatim: "\(output) (\(exectime) ms)")
+        NavigationStack {
+            VStack {
+                Text("Zenzai Japanese Input")
+                    .font(.title)
+                if !output.isEmpty {
+                    Text(verbatim: "\(output) (\(exectime) ms)")
+                }
+                TextField("type kana here", text: $input)
+                    .textFieldStyle(.roundedBorder)
             }
-            TextField("type kana here", text: $input)
-                .textFieldStyle(.roundedBorder)
-        }
-        .padding()
-        .onChange(of: input, initial: false) {
-            var c = ComposingText()
-            c.insertAtCursorPosition(input, inputStyle: .direct)
-            let s = Date()
-            let res = converter.requestCandidates(c, options: options)
-            self.output = res.mainResults.first?.text ?? "Error"
-            self.exectime = String(-s.timeIntervalSinceNow * 1000)
+            .padding()
+            .onChange(of: input, initial: false) {
+                var c = ComposingText()
+                c.insertAtCursorPosition(input, inputStyle: .direct)
+                let s = Date()
+                let res = converter.requestCandidates(c, options: options)
+                self.output = res.mainResults.first?.text ?? "Error"
+                self.exectime = String(-s.timeIntervalSinceNow * 1000)
+            }
+            .sheet(isPresented: $presentInspector) {
+                Form {
+                    Section {
+                        Toggle("Enable Zenzai", isOn: $zenzaiEnabled)
+                        Stepper("Inference Limit \(zenzaiInferenceLimit)", value: $zenzaiInferenceLimit, in: 1...10)
+                            .disabled(!self.zenzaiEnabled)
+                    }
+                }
+                .presentationDetents([.medium])
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Preferences", systemImage: "gearshape") {
+                        presentInspector.toggle()
+                    }
+                }
+            }
         }
     }
 
     private var options: ConvertRequestOptions {
-        .withDefaultDictionary(
+        let zenzaiMode: ConvertRequestOptions.ZenzaiMode = self.zenzaiEnabled ? .on(
+            weight: Bundle.main.bundleURL.appending(
+                path: "ggml-model-Q5_K_M.gguf"
+            ),
+            inferenceLimit: self.zenzaiInferenceLimit,
+            personalizationMode: nil
+        ) : .off
+        return .withDefaultDictionary(
             requireJapanesePrediction: false,
             requireEnglishPrediction: false,
             keyboardLanguage: .ja_JP,
             learningType: .nothing,
             memoryDirectoryURL: .applicationDirectory,
             sharedContainerURL: .applicationDirectory,
-            zenzaiMode: .on(
-                weight: Bundle.main.bundleURL.appending(
-                    path: "ggml-model-Q5_K_M.gguf"
-                ),
-                inferenceLimit: 1,
-                personalizationMode: nil
-            ),
+            zenzaiMode: zenzaiMode,
             metadata: nil
         )
     }
